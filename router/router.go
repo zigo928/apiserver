1176f9ffb65ff8bfe3cd37cb2cb394c1f86fddf2
gengzhiguo <gengzhiguo@xiongmaojinfu.com>
2018-08-06 17:45:08 +0800

    fix

--- router/router.go
+++ router/router.go
@@ -3,15 +3,21 @@ package router
 import (
 	"net/http"
 
+	_ "apiserver/docs"
 	"apiserver/handler/sd"
 	"apiserver/handler/user"
 	"apiserver/router/middleware"
 
+	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 // Load loads the middlewares, routes, handlers.
 func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// pprof router
+	pprof.Register(g)
 	// Middlewares.
 	g.Use(gin.Recovery())
 	g.Use(middleware.NoCache)
@@ -22,6 +28,8 @@ func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route.")
 	})
+
+	g.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	g.POST("/login", user.Login)
 
